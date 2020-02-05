e6f55759586298016d2facadc0c85bbb73a3d3a1
Adam Sällergård <adam.sallergard@griffeye.com>
2020-02-05 13:48:46 +0100

    Reproduce heap corruption.

---
+++ VlcMosaic/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        const int nrOfPlayers = 12;
+
+        readonly Player[] players;
+        public Form1()
+        {
+            InitializeComponent();
+
+            players = new Player[nrOfPlayers];
+
+            LibVLCSharp.Shared.Core.Initialize();
+
+            var libVlc = new LibVLCSharp.Shared.LibVLC(new[] {
+                "--verbose=2",
+                "--no-audio",
+            });
+
+            libVlc.SetLogFile("vlclog.txt");
+            for (int i = 0; i < nrOfPlayers; i++)
+            {
+                players[i] = new Player(((float)i) / nrOfPlayers, libVlc);
+            }
+            flowLayoutPanel1.Controls.AddRange(players.Select(x => x.Control).ToArray());
+        }
+
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            while (true)
+            {
+                var playTasks = players.Select(p => Task.Run(() =>
+                {
+                    p.Play(@"..\..\00020309.MPG");
+                }));
+                await Task.WhenAll(playTasks);
+            }
+        }
+    }
+}
---
+++ VlcMosaic/Player.cs
@@ -0,0 +1,49 @@
+﻿using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public sealed class Player : IDisposable
+    {
+        private readonly LibVLC libVLC;
+        private readonly VideoView videoView;
+        private readonly MediaPlayer mediaPlayer;
+        private readonly float start;
+
+        public Player(float start, LibVLC libVLC)
+        {
+            this.libVLC = libVLC;
+
+            videoView = new VideoView();
+            videoView.Size = new Size(400, 300);
+            mediaPlayer = new MediaPlayer(libVLC);
+            videoView.MediaPlayer = mediaPlayer;
+
+            this.start = start;
+        }
+
+        public Control Control => videoView;
+
+
+        public void Play(string file)
+        {
+            using (var media = new Media(libVLC, file, FromType.FromPath))
+            {
+                mediaPlayer.Media = media;
+            }
+            if (mediaPlayer.Play())
+            {
+                mediaPlayer.Position = start;
+            }
+        }
+
+        public void Dispose()
+        {
+            mediaPlayer.Dispose();
+            videoView.Dispose();
+        }
+    }
+}
